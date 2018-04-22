8d3a1e8b8c0a89a59d49b0ddcb33116f32e027cb
Arkham <allyoursanity@gmail.com>
2018-04-22 09:58:40 -0400

    digit multiplier returns expected values

--- isbn.rb
+++ isbn.rb
@@ -5,3 +5,12 @@ def isbn10 (params)
     return false
   end
 end
+
+def isbn_multiply_digits(params)
+  params = params.split(//).map(&:to_i)
+  result = Array.new
+  params.each_with_index do |digit, index|
+    result.push digit * (index + 1)
+  end
+  return result
+end
--- test_isbn.rb
+++ test_isbn.rb
@@ -16,4 +16,13 @@ class ISBN_Test < Minitest::Test
   def test_isbn10_length_check_false1
     assert_equal(false, isbn10("123456789ABCDE"))
   end
+
+  def test_isbnmultiplydigits_actually_multiplies1
+    assert_equal([1,4,9,16,25,36,49,64,81,0], isbn_multiply_digits("1234567890"))
+  end
+
+  def test_isbnmultiplydigits_actually_multiplies2
+    assert_equal([2,2,9,20,20,36,56,56,81,0], isbn_multiply_digits("2135468790"))
+  end
+
 end
