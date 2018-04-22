4ffdcd6ea0e35a2da84e50360f7ae8a4b9e84330
Arkham <allyoursanity@gmail.com>
2018-04-22 09:20:48 -0400

    isbn10 checks for correct length

---
+++ isbn.rb
@@ -0,0 +1,7 @@
+def isbn10 (params)
+  if params.length == 10
+    return true
+  else
+    return false
+  end
+end
--- test_isbn.rb
+++ test_isbn.rb
@@ -5,4 +5,15 @@ class ISBN_Test < Minitest::Test
   def test_int_is_int
     assert_equal(1.class, Integer)
   end
+
+  def test_isbn10_length_check_true
+    assert_equal(true, isbn10("123456789X"))
+  end
+
+  def test_isbn10_length_check_false1
+    assert_equal(false, isbn10("123"))
+  end
+  def test_isbn10_length_check_false1
+    assert_equal(false, isbn10("123456789ABCDE"))
+  end
 end
