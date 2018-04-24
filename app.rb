2bb34d2d1601e135bcbb52e290c76208b27fff62
Arkham <allyoursanity@gmail.com>
2018-04-24 14:05:17 -0400

    Validation page active.

--- app.rb
+++ app.rb
@@ -5,3 +5,15 @@ require_relative 'isbn.rb'
 get '/' do
   erb :check_if_valid
 end
+
+post '/check_input' do
+  isbn_status = process_isbn(params[:isbn_type], params[:isbn_value])
+  redirect '/validation?type=' + params[:isbn_type] + '&value=' + params[:isbn_value] + '&status=' + isbn_status.to_s
+end
+
+get '/validation' do
+  value = params[:value]
+  status = params[:status]
+  type = params[:type]
+  erb :validation, locals:{type:type,value:value,status:status}
+end
