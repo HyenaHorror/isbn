a0864c493c0cb4f113a62d7b73847a10349f71c2
Rob Bird <robertbirdwv@gmail.com>
2018-06-05 11:40:33 -0400

    Tested new user method.

--- psql.rb
+++ psql.rb
@@ -1,3 +1,5 @@
+require_relative 'bcrypt.rb'
+
 def read_last_isbn_in_database
   con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
 
@@ -9,4 +11,12 @@ def add_isbn_to_history(isbn, status, user)
   con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
 
   con.exec "INSERT INTO History VALUES(DEFAULT, '#{isbn}', '#{status}', '#{user}')"
+end
+
+def create_new_user(username, password)
+  con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  rs = con.exec "SELECT * FROM History ORDER BY Id DESC LIMIT 1"
+
+
+  passhash = hash_password(password)
 end
--- test_isbn.rb
+++ test_isbn.rb
@@ -197,4 +197,25 @@ class ISBN_Test < Minitest::Test
     actual = hash == password
     assert_equal(true, actual)
   end
+
+  def test_create_user_in_db
+    con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+
+    idnum = con.exec "SELECT id FROM Users ORDER BY Id DESC LIMIT 1"
+    id = idnum.to_a[0]["id"].to_i + 1
+    randnum = 10.times.map{Random.rand(0..9)}.join
+    name = ["Francis", "Hank", "Bill", "Zoey", "Dale", "Louis", "Boomhauer"].sample + randnum
+    a = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map(&:to_a).flatten
+    password = (0...16).map { a[rand(a.length)] }.join
+    passhash = hash_password(password)
+
+    con.exec "INSERT INTO Users VALUES(DEFAULT, '#{name}', '#{passhash}')"
+    rs = con.exec "SELECT * FROM Users ORDER BY Id DESC LIMIT 1"
+    actual = rs.to_a[0]
+    pass = BCrypt::Password.create(password, cost:4)
+    rescue BCrypt::Errors::InvalidHash
+
+    expected = {"id" => id.to_s, "username" => name, "passhash" => pass}
+    assert_equal(expected, actual)
+  end
 end
