35f5e6bd789bb4ebc6b41c7f1f207929a1793533
Rob Bird <robertbirdwv@gmail.com>
2018-06-06 23:49:20 -0400

    Parsing database url with URI

--- psql.rb
+++ psql.rb
@@ -3,38 +3,49 @@ require 'uri'
 
 load './local_env.rb' if File.exist?('./local_env.rb')
 def read_last_isbn_in_database
-  con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  # con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  uri = URI.parse(ENV['DATABASE_URL'])
+  con = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
 
   rs = con.exec "SELECT * FROM History ORDER BY Id DESC LIMIT 1"
   return rs.to_a
 end
 
 def add_isbn_to_history(isbn, status, user)
-  con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  # con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  uri = URI.parse(ENV['DATABASE_URL'])
+  con = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
 
   con.exec "INSERT INTO History VALUES(DEFAULT, '#{isbn}', '#{status}', '#{user}')"
 end
 
 def create_new_user(username, password)
-  con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  # con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  uri = URI.parse(ENV['DATABASE_URL'])
+  con = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
   passhash = hash_password(password)
   con.exec "INSERT INTO Users VALUES(DEFAULT, '#{username}', '#{passhash}')"
 end
 def verify_login_information(username, password)
-  con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  # con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  uri = URI.parse(ENV['DATABASE_URL'])
+  con = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
   rs = con.exec "SELECT passhash FROM Users WHERE username = #{'username'} LIMIT 1"
   passhash = BCrypt::Password.new rs.to_a[0]["passhash"]
   return passhash == password
 end
 
 def is_username_in_use(username)
-  con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  # con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  uri = URI.parse(ENV['DATABASE_URL'])
+  con = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
   rs = con.exec "SELECT username FROM Users WHERE username = '#{username}' LIMIT 1"
   rs.to_a[0] != nil
 end
 
 def get_full_history
-  con = PG.connect(:dbname => ENV['DBNAME'], :user => ENV['DBUSER'], :password => ENV['DBPASS'])
+  uri = URI.parse(ENV['DATABASE_URL'])
+  con = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
 
   rs = con.exec "SELECT * FROM History"
   return rs.to_a
